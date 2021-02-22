b047b49405256d990da3a53f0935eb4168a2a9e4
Yangying <ryysophie@126.com>
2021-02-23 06:06:38 +0800

    Elevator Test

--- USSR/Assets/Scripts/Gravity.cs
+++ USSR/Assets/Scripts/Gravity.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Gravity : MonoBehaviour {
-  float FORCE_OF_GRAVITY = 100F;
+  float FORCE_OF_GRAVITY = 10F;
   
   void Start () {
     Debug.Log("Start");
@@ -13,12 +13,26 @@ public class Gravity : MonoBehaviour {
     if (Input.GetMouseButtonDown(0)) {
       if (Physics.gravity.y > 0) {
         // Normal gravity so downward force of -9.8
-        Physics.gravity = new Vector3(0, FORCE_OF_GRAVITY * (-1), 0);
+        Physics.gravity = new Vector3(FORCE_OF_GRAVITY, 0, 0);
         gameObject.GetComponent<MouseLook>().enabled=false;
-        gameObject.transform.Rotate(180.0f, 0.0f, 0.0f, Space.World);
-      } else {
+        gameObject.transform.Rotate(0.0f, 0.0f, 90.0f, Space.Self);
+      } 
+      else if(Physics.gravity.x>0&&Physics.gravity.y==0){
+        Physics.gravity = new Vector3(0,FORCE_OF_GRAVITY*(-1.0f), 0);
+        gameObject.GetComponent<MouseLook>().enabled=false;
+        gameObject.transform.Rotate(0.0f, 0.0f, 90.0f, Space.Self);
+
+      }
+      else if (Physics.gravity.y<0){
+        Physics.gravity = new Vector3(FORCE_OF_GRAVITY*(-1.0f),0,0);
+        gameObject.GetComponent<MouseLook>().enabled=false;
+        gameObject.transform.Rotate(0.0f, 0.0f, 90.0f, Space.Self);
+      }
+      else {
         // Inverse gravity
         Physics.gravity = new Vector3(0, FORCE_OF_GRAVITY, 0);
+        gameObject.GetComponent<MouseLook>().enabled=true;
+        gameObject.transform.Rotate(0.0f, 0.0f, 90.0f, Space.Self);
       }
     }
   }
